2cfbff26197ebfdc244f419df4f741a08c3818c9
HMarraha <marrahahaytam@gmail.com>
2023-05-23 19:26:17 +0100

    Added responsiveness for navbar and added a dropdown box for latest

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import Landing from './Views/Landing'
 import './App.css'
 
 function App() {
+  const [latest,setLatest] = useState(false)
+    const handleLatest = () => {
+        setLatest(prevLatest => !prevLatest)
+    }
   return (
     <>
-      <Landing />
+      <Landing latest={latest} handleLatest={handleLatest} />
     </>  
   )
 }
--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -1,8 +1,14 @@
 import {useState} from "react"
+import {FaBars} from "react-icons/fa"
 import logo from "../assets/logo.jpg"
-export default function Landing() {
-
-
+import "../index.css"
+export default function Landing({latest,handleLatest}) {
+    const styles = {
+        visibility: latest ? "hidden" : "visible",
+        transform: latest ? "translateY(-5%)" : "translateY(5%)",
+        transition: latest ? "0s" : "150ms ease-in"
+    }
+    console.log(latest)
   return (
     <>
         <header>
@@ -10,18 +16,23 @@ export default function Landing() {
                 <a href='#'><img className='logo' src={logo} alt="Logo" /></a>
                 <div className='nav-links'> 
                     <ul className='nav-links'>
-                        <li><a className='links navbar-links' href="#">Latest</a></li>
+                        <li><a onClick={handleLatest} className='links navbar-links' href="#">Latest</a></li>
                         <li><a className='links navbar-links' href="#">About</a></li>
                         <li><a className='links navbar-links' href="#">FAQ</a></li>
                     </ul>
                     <button className='navbar-btn' type='button'>Login</button>
                     <button className='navbar-btn' type='button'>Signup</button>
-                    
+                    <i className="hamburger"><FaBars size="30px" color="white" /></i>   
                 </div>
             </nav>
         </header>
         <main>
             <div className='hero'>
+                <div style={styles} className="latestmtv">
+                    <p><a href="#">Popular</a></p>
+                    <p><a href="#">Movies</a></p>
+                    <p><a href="#">Tv-shows</a></p>
+                </div>
                 <div className='hero-text'>
                     <h1>Welcome to MyMoviesList! You can track how many movies you've watched, still watching, and want to watch.</h1>
                     <p>There are many features you can use glad to MyMoviesList such as adding ratings, comments and many more.</p>

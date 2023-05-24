160a9083476f1438f8098b3ff0d783bc9fef0b36
HMarraha <marrahahaytam@gmail.com>
2023-05-24 02:12:27 +0100

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import './App.css'
 
 function App() {
   const [latest,setLatest] = useState(false)
+  const [icon,setIcon] = useState(false)
     const handleLatest = () => {
-        setLatest(prevLatest => !prevLatest)
+      setLatest(prevLatest => !prevLatest)
     }
+    const handleIcon = () => {
+      setIcon(prevIcon => !prevIcon)
+    }
+  
   return (
     <>
-      <Landing latest={latest} handleLatest={handleLatest} />
+      <Landing latest={latest} handleLatest={handleLatest} icon={icon} handleIcon={handleIcon} />
     </>  
   )
 }
--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -1,14 +1,19 @@
 import {useState} from "react"
-import {FaBars} from "react-icons/fa"
+import {FaBars,FaTimes} from "react-icons/fa"
 import logo from "../assets/logo.jpg"
 import "../index.css"
-export default function Landing({latest,handleLatest}) {
+export default function Landing({latest,handleLatest,icon,handleIcon}) {
     const styles = {
         visibility: latest ? "hidden" : "visible",
         transform: latest ? "translateY(-5%)" : "translateY(5%)",
-        transition: latest ? "0s" : "150ms ease-in"
+        transition: latest ? "0s" : "150ms ease-out",
+        opacity: latest ? "0" : "1",
     }
-    console.log(latest)
+    const translate = {
+        transform: icon ? "translateX(0rem)" : "translate(30rem)",
+        transition: "350ms ease-in"
+    }
+    console.log(icon)
   return (
     <>
         <header>
@@ -16,23 +21,32 @@ export default function Landing({latest,handleLatest}) {
                 <a href='#'><img className='logo' src={logo} alt="Logo" /></a>
                 <div className='nav-links'> 
                     <ul className='nav-links'>
+                        <div className="dropdown">
                         <li><a onClick={handleLatest} className='links navbar-links' href="#">Latest</a></li>
-                        <li><a className='links navbar-links' href="#">About</a></li>
-                        <li><a className='links navbar-links' href="#">FAQ</a></li>
+                         <div style={styles} className="latest">
+                            <li><a href="#">Popular</a></li>  
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV Shows</a></li>
+                         </div>
+                        </div>
+                            <li><a className='links navbar-links' href="#">About</a></li>
+                            <li><a className='links navbar-links' href="#">FAQS</a></li>
+                        <div style={translate} className="mobilenav">
+                            <li><a onClick={handleLatest} className='link navbar-link' href="#">Latest</a></li>
+                            <li><a className='link navbar-link' href="#">About</a></li>
+                            <li><a className='link navbar-link' href="#">FAQS</a></li>
+                            <button className='mobile-btn' type='button'>Login</button>
+                            <button className='mobile-btn' type='button'>Sign up</button>
+                        </div>
                     </ul>
                     <button className='navbar-btn' type='button'>Login</button>
-                    <button className='navbar-btn' type='button'>Signup</button>
-                    <i className="hamburger"><FaBars size="30px" color="white" /></i>   
+                    <button className='navbar-btn' type='button'>Sign up</button>
+                    <i onClick={handleIcon} className="hamburger">{icon ? <FaTimes size="30px" color="white"/> : <FaBars size="30px" color="white" />}</i>   
                 </div>
             </nav>
         </header>
         <main>
             <div className='hero'>
-                <div style={styles} className="latestmtv">
-                    <p><a href="#">Popular</a></p>
-                    <p><a href="#">Movies</a></p>
-                    <p><a href="#">Tv-shows</a></p>
-                </div>
                 <div className='hero-text'>
                     <h1>Welcome to MyMoviesList! You can track how many movies you've watched, still watching, and want to watch.</h1>
                     <p>There are many features you can use glad to MyMoviesList such as adding ratings, comments and many more.</p>
